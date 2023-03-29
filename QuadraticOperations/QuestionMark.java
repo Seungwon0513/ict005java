68691a946f0823a0e43b745b1ab31f0135195ab8
Seungwon <128570762+Seungwon0513@users.noreply.github.com>
2023-03-29 23:44:20 +0900

    Add files via upload

---
+++ QuadraticOperations/QuestionMark.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+
+public class QuestionMark {
+
+		public static void main (String[] args) {
+			Scanner sc = new Scanner (System.in);
+			
+			String lastString =  "??!";
+			
+			String s = sc.next();
+			System.out.println(s + lastString);
+			
+			}
+}
