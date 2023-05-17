a03016b820c2bb06fea82646a1063d762da77d56
Seungwon <128570762+Seungwon0513@users.noreply.github.com>
2023-05-17 19:08:36 +0900

    Add files via upload

---
+++ MakingWindowSample/MakingWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+
+
+public class MakingWindow {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Frame Test");
+
+		f.setSize(300, 200);
+		f.setVisible(true);
+	}
+}
